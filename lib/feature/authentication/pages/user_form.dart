97f7b5deaaf76f54e235774a9acdd2037b9199e5
Ryandi Aziz <ryandiaziz.edu@gmail.com>
2023-01-19 11:19:41 +0700

    Menambahkan widget date picker

--- lib/feature/authentication/pages/user_form.dart
+++ lib/feature/authentication/pages/user_form.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_menu_app/common/widgets/custom_elevated_button.dart';
 import 'package:e_menu_app/common/widgets/custom_textfield.dart';
+import 'package:e_menu_app/feature/authentication/widgets/date_picker.dart';
 import 'package:e_menu_app/feature/authentication/widgets/sex_drop_down.dart';
 import 'package:e_menu_app/shared/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -25,20 +26,6 @@ class _UserFormState extends State<UserForm> {
 
   // List of items in our dropdown menu
   var items = ['Male', 'Female'];
-
-  Future<void> _selectDateFromPicker(BuildContext context) async {
-    final DateTime? picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime(DateTime.now().year - 20),
-      firstDate: DateTime(DateTime.now().year - 30),
-      lastDate: DateTime(DateTime.now().year),
-    );
-    if (picked != null) {
-      setState(() {
-        dobController.text = "${picked.day}/ ${picked.month}/ ${picked.year}";
-      });
-    }
-  }
 
   sendUserDataToDB() async {
     final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -110,52 +97,7 @@ class _UserFormState extends State<UserForm> {
                   keyBoardType: TextInputType.number,
                   read: false,
                 ),
-                Container(
-                  margin: const EdgeInsets.only(top: 20),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        height: 50,
-                        padding: const EdgeInsets.symmetric(horizontal: 16),
-                        decoration: BoxDecoration(
-                          color: const Color(0xffEFF0F6),
-                          borderRadius: BorderRadius.circular(5),
-                        ),
-                        child: Center(
-                          child: Row(
-                            children: [
-                              Image.asset(
-                                'assets/icon/calendar.png',
-                                width: 17,
-                                color: secondsubtitleColor,
-                              ),
-                              const SizedBox(
-                                width: 16,
-                              ),
-                              Expanded(
-                                child: TextFormField(
-                                  readOnly: true,
-                                  controller: dobController,
-                                  style: primaryTextStyle,
-                                  decoration: InputDecoration(
-                                    hintText: "date of birth",
-                                    hintStyle: subtitleTextStyle,
-                                    suffixIcon: IconButton(
-                                      onPressed: () =>
-                                          _selectDateFromPicker(context),
-                                      icon: const Icon(Icons.arrow_drop_down),
-                                    ),
-                                  ),
-                                ),
-                              )
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                DatePicker(dobController: dobController),
                 SexDropDown(gender: gender, items: items),
                 const SizedBox(height: 20),
                 CustomElevatedButton(
